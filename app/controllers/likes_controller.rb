37d10fe0e5ea49e451feaae3e161681e859a9d5a
Babin Bohora <bohorababin1@gmail.com>
2022-01-15 08:14:37 +0900

    like create method looking gooood. Need some validation

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,10 @@
 class LikesController < ApplicationController
   def create
     @user = current_user
+    @article = Article.find(params[:article_id])
+    Like.create(user: @user, article: @article)
+    redirect_to request.referer
+    # raise
   end
 
   def destroy
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :article
+
+  validates :user, presence: true
+  validates :article, presence: true
+  # should have unique combination of user and article for each like
 end
